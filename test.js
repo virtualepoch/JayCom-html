6c2e713d907e83f23248e5b2853614130f901e2a
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-03-15 19:01:40 -0600

    3-15-22 7pm update
    finished menu box and js for it

---
+++ test.js
@@ -0,0 +1,9 @@
+var main_menu = document.getElementById("main-menu-container");
+
+function openMainMenu(){
+    if(main_menu.style.display== "none"){
+        main_menu.style.display="";
+    }else{
+        main_menu.style.display="none";
+    }
+}
