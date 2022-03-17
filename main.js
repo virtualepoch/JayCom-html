b8ac7bf10be6dd1b3f28da442c147876e335ae88
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-03-17 16:11:25 -0600

    3/17/22 4pm update
    Added script for remaining table of contents dropdowns. Messed around with page layout and scrolling.

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ function openTableContents() {
   }
 }
 
+// ==TABLE=OF=CONTENTS=CONTAINER==GUIDE=1=EXPAND=FUNCTIONS==VVVVVVVVVVVVVVVVVVVVVVV
 var guide_1_contents = document.getElementById("table-contents-articles-container-1");
 var guide_arrow_1 = document.getElementById("guide-arrow-1");
 
@@ -56,5 +57,197 @@ function expandArticle_2() {
   }
 }
 
+// ==TABLE=OF=CONTENTS=CONTAINER==GUIDE=2=EXPAND=FUNCTIONS==VVVVVVVVVVVVVVVVVVVVVVV
+var guide_2_contents = document.getElementById("table-contents-articles-container-2");
+var guide_arrow_2 = document.getElementById("guide-arrow-2");
 
+function expandGuide_2() {
+  if (guide_2_contents.style.display == "") {
+    guide_2_contents.style.display = "block";
+    guide_arrow_2.style.transform = "rotate(90deg)";
+  } else {
+    guide_2_contents.style.display = "";
+    guide_arrow_2.style.transform = "none";
+  }
+}
 
+var article_3_contents = document.getElementById("table-contents-links-container-3");
+var article_arrow_3 = document.getElementById("article-arrow-3");
+
+function expandArticle_3() {
+  if (article_3_contents.style.display == "") {
+    article_3_contents.style.display = "block";
+    article_arrow_3.style.transform = "rotate(90deg)";
+  } else {
+    article_3_contents.style.display = "";
+    article_arrow_3.style.transform = "none";
+  }
+}
+var article_4_contents = document.getElementById("table-contents-links-container-4");
+var article_arrow_4 = document.getElementById("article-arrow-4");
+
+function expandArticle_4() {
+  if (article_4_contents.style.display == "") {
+    article_4_contents.style.display = "block";
+    article_arrow_4.style.transform = "rotate(90deg)";
+  } else {
+    article_4_contents.style.display = "";
+    article_arrow_4.style.transform = "none";
+  }
+}
+
+// ==TABLE=OF=CONTENTS=CONTAINER==GUIDE=3=EXPAND=FUNCTIONS==VVVVVVVVVVVVVVVVVVVVVVV
+var guide_3_contents = document.getElementById("table-contents-articles-container-3");
+var guide_arrow_3 = document.getElementById("guide-arrow-3");
+
+function expandGuide_3() {
+  if (guide_3_contents.style.display == "") {
+    guide_3_contents.style.display = "block";
+    guide_arrow_3.style.transform = "rotate(90deg)";
+  } else {
+    guide_3_contents.style.display = "";
+    guide_arrow_3.style.transform = "none";
+  }
+}
+
+var article_5_contents = document.getElementById("table-contents-links-container-5");
+var article_arrow_5 = document.getElementById("article-arrow-5");
+
+function expandArticle_5() {
+  if (article_5_contents.style.display == "") {
+    article_5_contents.style.display = "block";
+    article_arrow_5.style.transform = "rotate(90deg)";
+  } else {
+    article_5_contents.style.display = "";
+    article_arrow_5.style.transform = "none";
+  }
+}
+var article_6_contents = document.getElementById("table-contents-links-container-6");
+var article_arrow_6 = document.getElementById("article-arrow-6");
+
+function expandArticle_6() {
+  if (article_6_contents.style.display == "") {
+    article_6_contents.style.display = "block";
+    article_arrow_6.style.transform = "rotate(90deg)";
+  } else {
+    article_6_contents.style.display = "";
+    article_arrow_6.style.transform = "none";
+  }
+}
+
+// ==TABLE=OF=CONTENTS=CONTAINER==GUIDE=4=EXPAND=FUNCTIONS==VVVVVVVVVVVVVVVVVVVVVVV
+var guide_4_contents = document.getElementById("table-contents-articles-container-4");
+var guide_arrow_4 = document.getElementById("guide-arrow-4");
+
+function expandGuide_4() {
+  if (guide_4_contents.style.display == "") {
+    guide_4_contents.style.display = "block";
+    guide_arrow_4.style.transform = "rotate(90deg)";
+  } else {
+    guide_4_contents.style.display = "";
+    guide_arrow_4.style.transform = "none";
+  }
+}
+
+var article_7_contents = document.getElementById("table-contents-links-container-7");
+var article_arrow_7 = document.getElementById("article-arrow-7");
+
+function expandArticle_7() {
+  if (article_7_contents.style.display == "") {
+    article_7_contents.style.display = "block";
+    article_arrow_7.style.transform = "rotate(90deg)";
+  } else {
+    article_7_contents.style.display = "";
+    article_arrow_7.style.transform = "none";
+  }
+}
+var article_8_contents = document.getElementById("table-contents-links-container-8");
+var article_arrow_8 = document.getElementById("article-arrow-8");
+
+function expandArticle_8() {
+  if (article_8_contents.style.display == "") {
+    article_8_contents.style.display = "block";
+    article_arrow_8.style.transform = "rotate(90deg)";
+  } else {
+    article_8_contents.style.display = "";
+    article_arrow_8.style.transform = "none";
+  }
+}
+
+// ==TABLE=OF=CONTENTS=CONTAINER==GUIDE=5=EXPAND=FUNCTIONS==VVVVVVVVVVVVVVVVVVVVVVV
+var guide_5_contents = document.getElementById("table-contents-articles-container-5");
+var guide_arrow_5 = document.getElementById("guide-arrow-5");
+
+function expandGuide_5() {
+  if (guide_5_contents.style.display == "") {
+    guide_5_contents.style.display = "block";
+    guide_arrow_5.style.transform = "rotate(90deg)";
+  } else {
+    guide_5_contents.style.display = "";
+    guide_arrow_5.style.transform = "none";
+  }
+}
+
+var article_9_contents = document.getElementById("table-contents-links-container-9");
+var article_arrow_9 = document.getElementById("article-arrow-9");
+
+function expandArticle_9() {
+  if (article_9_contents.style.display == "") {
+    article_9_contents.style.display = "block";
+    article_arrow_9.style.transform = "rotate(90deg)";
+  } else {
+    article_9_contents.style.display = "";
+    article_arrow_9.style.transform = "none";
+  }
+}
+var article_10_contents = document.getElementById("table-contents-links-container-10");
+var article_arrow_10 = document.getElementById("article-arrow-10");
+
+function expandArticle_10() {
+  if (article_10_contents.style.display == "") {
+    article_10_contents.style.display = "block";
+    article_arrow_10.style.transform = "rotate(90deg)";
+  } else {
+    article_10_contents.style.display = "";
+    article_arrow_10.style.transform = "none";
+  }
+}
+
+// ==TABLE=OF=CONTENTS=CONTAINER==GUIDE=6=EXPAND=FUNCTIONS==VVVVVVVVVVVVVVVVVVVVVVV
+var guide_6_contents = document.getElementById("table-contents-articles-container-6");
+var guide_arrow_6 = document.getElementById("guide-arrow-6");
+
+function expandGuide_6() {
+  if (guide_6_contents.style.display == "") {
+    guide_6_contents.style.display = "block";
+    guide_arrow_6.style.transform = "rotate(90deg)";
+  } else {
+    guide_6_contents.style.display = "";
+    guide_arrow_6.style.transform = "none";
+  }
+}
+
+var article_11_contents = document.getElementById("table-contents-links-container-11");
+var article_arrow_11 = document.getElementById("article-arrow-11");
+
+function expandArticle_11() {
+  if (article_11_contents.style.display == "") {
+    article_11_contents.style.display = "block";
+    article_arrow_11.style.transform = "rotate(90deg)";
+  } else {
+    article_11_contents.style.display = "";
+    article_arrow_11.style.transform = "none";
+  }
+}
+var article_12_contents = document.getElementById("table-contents-links-container-12");
+var article_arrow_12 = document.getElementById("article-arrow-12");
+
+function expandArticle_12() {
+  if (article_12_contents.style.display == "") {
+    article_12_contents.style.display = "block";
+    article_arrow_12.style.transform = "rotate(90deg)";
+  } else {
+    article_12_contents.style.display = "";
+    article_arrow_12.style.transform = "none";
+  }
+}
