734a10e3cecbca3c99f5c4dc73a959eb0c057fd7
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2022-03-19 22:33:53 -0600

    3/19/22 10pm update
    -added some filler images
    -messed around with the intersection observer
    -added bob ross video

--- guides.js
+++ guides.js
@@ -1,21 +1,20 @@
-// // ==ON=LOAD==IN=THIS=ARTICLE==STYLE=CHANGE=(ADD ACTIVE CLASS ON PAGE LOAD)==
-// var articleSectionTab_1 = document.getElementById("article-section-1-tab");
-
-// window.onload = function articleTabStyle() {
-//   articleSectionTab_1.classList.add("active");
-// };
 
 // ==SCROLL=CHANGE=IN=THIS=ARTICLE=STYLING==SECTION_1==///////////////
-const section_1 = document.querySelector("#article-1-section-1-text");
+const section_1 = document.querySelector("#article-1-section-1-content-box");
+const contentsLink_1 = document.querySelector("#table-contents-link-1");
 const articleSectionTab_1 = document.querySelector("#article-section-1-tab");
 
 const navObserver = new IntersectionObserver(
   (entries, observer) => {
     const entry = entries[0];
     if (!entry.isIntersecting) {
+        contentsLink_1.classList.remove("active");
       articleSectionTab_1.classList.remove("active");
+      
     } else {
+        contentsLink_1.classList.add("active");
       articleSectionTab_1.classList.add("active");
+      
     }
   },
   {
@@ -25,15 +24,18 @@ const navObserver = new IntersectionObserver(
 navObserver.observe(section_1);
 
 // ==SCROLL=CHANGE=IN=THIS=ARTICLE=STYLING==SECTION_2==///////////////
-const section_2 = document.querySelector("#article-1-section-2-text");
+const section_2 = document.querySelector("#article-1-section-2-content-box");
+const contentsLink_2 = document.querySelector("#table-contents-link-2");
 const articleSectionTab_2 = document.querySelector("#article-section-2-tab");
 
 const navObserver2 = new IntersectionObserver(
   (entries, observer) => {
     const entry = entries[0];
     if (!entry.isIntersecting) {
+        contentsLink_2.classList.remove("active");
       articleSectionTab_2.classList.remove("active");
     } else {
+        contentsLink_2.classList.add("active");
       articleSectionTab_2.classList.add("active");
     }
   },
@@ -44,15 +46,18 @@ const navObserver2 = new IntersectionObserver(
 navObserver2.observe(section_2);
 
 // ==SCROLL=CHANGE=IN=THIS=ARTICLE=STYLING==SECTION_3==///////////////
-const section_3 = document.querySelector("#article-1-section-3-text");
+const section_3 = document.querySelector("#article-1-section-3-content-box");
+const contentsLink_3 = document.querySelector("#table-contents-link-3");
 const articleSectionTab_3 = document.querySelector("#article-section-3-tab");
 
 const navObserver3 = new IntersectionObserver(
   (entries, observer) => {
     const entry = entries[0];
     if (!entry.isIntersecting) {
+        contentsLink_3.classList.remove("active");
       articleSectionTab_3.classList.remove("active");
     } else {
+        contentsLink_3.classList.add("active");
       articleSectionTab_3.classList.add("active");
     }
   },
@@ -63,15 +68,18 @@ const navObserver3 = new IntersectionObserver(
 navObserver3.observe(section_3);
 
 // ==SCROLL=CHANGE=IN=THIS=ARTICLE=STYLING==SECTION_3==///////////////
-const section_4 = document.querySelector("#article-1-section-4-text");
+const section_4 = document.querySelector("#article-1-section-4-content-box");
+const contentsLink_4 = document.querySelector("#table-contents-link-4");
 const articleSectionTab_4 = document.querySelector("#article-section-4-tab");
 
 const navObserver4 = new IntersectionObserver(
   (entries, observer) => {
     const entry = entries[0];
     if (!entry.isIntersecting) {
+        contentsLink_4.classList.remove("active");
       articleSectionTab_4.classList.remove("active");
     } else {
+        contentsLink_4.classList.add("active");
       articleSectionTab_4.classList.add("active");
     }
   },
@@ -91,3 +99,22 @@ navObserver4.observe(section_4);
 // },{});
 
 // observer.observe(section_2);
+
+
+// ==FUN=WITH=ROCKET=TEXT=BOX==///////////////////
+
+const sectionImg = document.querySelector(".article-section-img");
+const rocketChat = document.querySelector("#rocket-chat-icon")
+
+const navObserver5 = new IntersectionObserver((entries, observer)=>{
+    const entry = entries[0];
+    if (!entry.isIntersecting){
+        rocketChat.classList.remove("active");
+    }else{
+        rocketChat.classList.add("active");
+    }
+},{
+      threshold: .3,
+}
+);
+navObserver5.observe(sectionImg);
